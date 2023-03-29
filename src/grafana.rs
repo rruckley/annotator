aa72873b186d5b7c70f1b9f2be0851b966707598
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-03-29 17:10:45 +1100

    Added logging

--- src/grafana.rs
+++ src/grafana.rs
@@ -1,11 +1,14 @@
 // Interact with Grafan to create annotations
 
+use log::{info};
+
 pub struct GrafanaClient {
 
 }
 
 impl GrafanaClient {
     pub fn new() -> Self {
+        info!("Creating new GrafanaClient()");
         Self {}
     }
 }
--- src/kafka.rs
+++ src/kafka.rs
@@ -1,11 +1,12 @@
 // Pull notifications from Kafka topic
-
+use log::{info};
 pub struct KafkaClient {
 
 }
 
 impl KafkaClient {
     pub fn new() -> Self {
+        info!("Creating new KafkaClient()");
         Self {}
     }
 }
