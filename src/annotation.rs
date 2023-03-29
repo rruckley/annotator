423f478cae5d5fe77aed0ac570763260391c59e4
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-03-30 10:16:43 +1100

    Tpye conversion

--- src/annotation.rs
+++ src/annotation.rs
@@ -1,4 +1,5 @@
 // Annotatoin module
+use crate::event::Event;
 
 pub struct Annotation {
     dashboard_id : Option<u32>,
@@ -7,4 +8,20 @@ pub struct Annotation {
     time_end : u64,
     tags : Vec<String>,
     text : String,
+}
+
+impl From<Event> for Annotation {
+    fn from(e: Event) -> Self {
+        Annotation {
+            dashboard_id : Some(0),
+            panel_id  : Some(0),
+            text : e.ticket_number.unwrap_or("No ticket".to_string()),
+            time : 0,
+            time_end : 0,
+            tags : vec![
+                "host=test".to_owned(),
+                "cust=test".to_owned()
+            ]
+        }
+    }
 }
