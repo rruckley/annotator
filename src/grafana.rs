a33fb819636605a71af2c3ec8c4eaffcd9c397cd
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-03-30 09:14:06 +1100

    new func

--- src/grafana.rs
+++ src/grafana.rs
@@ -2,6 +2,7 @@
 
 use log::{info};
 use crate::config::Config;
+use crate::annotation::Annotation;
 
 pub struct GrafanaClient {
 
@@ -12,5 +13,10 @@ impl GrafanaClient {
         let url = Config::get("GRAFANA_URL");
         info!("Creating new GrafanaClient({url})");
         Self {}
+    }
+
+    pub fn annotate(annotation : Annotation) -> Result<String,String> {
+        // Create supplied annotation in Grafana
+        Err("Not implemented".to_owned())
     }
 }
