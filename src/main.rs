7fae22835205023083ae54030d22aa71598e2d8a
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-03-29 16:37:12 +1100

    Create basic structs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@
 mod kafka;
 mod grafana;
 
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    info!("Grafana Annotator");
+
+    let kafka_client = kafka::KafkaClient::new();
+    let grafana_client = grafana::GrafanaClient::new();
 }
