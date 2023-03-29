5f178f08b8539f7ff252df26969b420c938b4e6f
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-03-29 17:32:39 +1100

    use config mod

--- src/grafana.rs
+++ src/grafana.rs
@@ -1,6 +1,7 @@
 // Interact with Grafan to create annotations
 
 use log::{info};
+use crate::config::Config;
 
 pub struct GrafanaClient {
 
@@ -8,7 +9,8 @@ pub struct GrafanaClient {
 
 impl GrafanaClient {
     pub fn new() -> Self {
-        info!("Creating new GrafanaClient()");
+        let url = Config::get("GRAFANA_URL");
+        info!("Creating new GrafanaClient({url})");
         Self {}
     }
 }
