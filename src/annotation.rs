b3f2fff5abc1b797c64f296a6ba1e8c2425e21a6
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-04-18 09:52:46 +1000

    generate tags

--- src/annotation.rs
+++ src/annotation.rs
@@ -5,22 +5,27 @@ pub struct Annotation {
     pub dashboard_id : Option<u32>,
     pub panel_id : Option<u32>,
     pub time : u64,
-    pub time_end : u64,
+    pub time_end : Option<u64>,
     pub tags : Vec<String>,
     pub text : String,
 }
 
 impl From<Event> for Annotation {
     fn from(e: Event) -> Self {
+        // Need to create tags representing customer and service
+        let customer = e.customer;
+        let service = e.service;
+        let timestamp = e.start_time;
+        let time_end = e.finish_time.map(|t| 0);
         Annotation {
             dashboard_id : Some(0),
             panel_id  : Some(0),
             text : e.ticket_number.unwrap_or("No ticket".to_string()),
             time : 0,
-            time_end : 0,
+            time_end : time_end,
             tags : vec![
-                "host=test".to_owned(),
-                "cust=test".to_owned(),
+                format!("host={}",service),
+                format!("customer={}",customer),
             ]
         }
     }
